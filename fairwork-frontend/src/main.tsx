191f599a9bb68a0c309930f838f99c5d5b863514
Suriya528 <e.suriya528@gmail.com>
2026-08-23 22:07:47 +0530

    refactor: remove StrictMode wrapper from main.tsx

--- fairwork-frontend/src/main.tsx
+++ fairwork-frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { App } from "@/App"
@@ -10,9 +9,7 @@ if (!rootElement) {
 }
 
 createRoot(rootElement).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 )
